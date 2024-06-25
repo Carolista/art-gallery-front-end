ee5fea2e8c4f5abf879c4c1898b4d22ae060e084
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-25 01:23:13 -0400

    linted react files

--- react/art-gallery-public/src/components/Card.jsx
+++ react/art-gallery-public/src/components/Card.jsx
@@ -1,12 +1,13 @@
 const Card = ({ artwork }) => {
-
-  return (
-    <div className="card artwork-card">
-      <img className="card-image" src={artwork.getImageURL()} />
-      <h5>{artwork.title}</h5>
-      <p>{artwork.artist} ({artwork.yearCreated})</p> 
-    </div>
-  )
-}
+	return (
+		<div className="card artwork-card">
+			<img className="card-image" src={artwork.getImageURL()} />
+			<h5>{artwork.title}</h5>
+			<p>
+				{artwork.artist} ({artwork.yearCreated})
+			</p>
+		</div>
+	);
+};
 
 export default Card;
--- react/art-gallery-public/src/components/Details.jsx
+++ react/art-gallery-public/src/components/Details.jsx
@@ -1,47 +1,56 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const Details = ({ artworks }) => {
+	const { id } = useParams();
 
-  const { id } = useParams();
+	const artwork = artworks[id];
 
-  const artwork = artworks[id];
-
-  return (
-    <main>
-      <p><a href="/artworks">&larr; Back to Artworks Gallery View</a></p>
-      <h2>ARTWORK DETAILS</h2>
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <img className="card-image" src={artwork.getImageURL()} /> 
-          </div>
-          <div className="col-6">
-            <h4 className="mb-4 mt-2 font-bold">{artwork.title + " (" + artwork.yearCreated + ")"}</h4>
-            <>
-              <h5>Artist</h5>
-              <p>{artwork.artist}</p>       
-            </>
-            {artwork.description != '' && <>
-              <h5>Description</h5>
-              <p>{artwork.description}</p> 
-            </>}
-            <>
-              <h5>Categories</h5>
-              <p>{artwork.categories}</p> 
-            </>
-            {artwork.media != '' && <>
-              <h5>Media</h5>
-              <p>{artwork.media}</p> 
-            </>}
-            {artwork.dimensions != '' && <>
-              <h5>Dimensions</h5>
-              <p>{artwork.dimensions}</p> 
-            </>}
-          </div>
-        </div>
-      </div>
-    </main>
-  )
+	return (
+		<main>
+			<p>
+				<a href="/artworks">&larr; Back to Artworks Gallery View</a>
+			</p>
+			<h2>ARTWORK DETAILS</h2>
+			<div className="container">
+				<div className="row">
+					<div className="col-6">
+						<img className="card-image" src={artwork.getImageURL()} />
+					</div>
+					<div className="col-6">
+						<h4 className="mb-4 mt-2 font-bold">
+							{artwork.title + ' (' + artwork.yearCreated + ')'}
+						</h4>
+						<>
+							<h5>Artist</h5>
+							<p>{artwork.artist}</p>
+						</>
+						{artwork.description != '' && (
+							<>
+								<h5>Description</h5>
+								<p>{artwork.description}</p>
+							</>
+						)}
+						<>
+							<h5>Categories</h5>
+							<p>{artwork.categories}</p>
+						</>
+						{artwork.media != '' && (
+							<>
+								<h5>Media</h5>
+								<p>{artwork.media}</p>
+							</>
+						)}
+						{artwork.dimensions != '' && (
+							<>
+								<h5>Dimensions</h5>
+								<p>{artwork.dimensions}</p>
+							</>
+						)}
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Details;
